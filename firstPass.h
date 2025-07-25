8e62c4612b06c31dff3bd4c47b5287137756862d
eilamLvsInhrtnc <gaziteilam1@gmail.com>
2025-07-25 13:12:59 +0300

    Create firstPass.h

---
+++ firstPass.h
@@ -0,0 +1,18 @@
+#ifndef FIRSTPASS_H
+#define FIRSTPASS_H
+
+typedef struct Symbol {
+    char *label;
+    int adress;
+    char *labelType;
+}Symbol;
+
+int isLabel(char* , char** , Symbol*);
+int isOpcode(char*);
+int isReg(char*);
+int countWordsForCode(char *);
+int countWordsForData(char *);
+int checkDupe(Symbol* , int , char*);
+Symbol* firstPass(int argc , char *argv[]);
+
+#endif
