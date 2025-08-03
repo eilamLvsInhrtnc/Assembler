f34691f099f670877b2776229c6f9dae6729b64d
eilamLvsInhrtnc <gaziteilam1@gmail.com>
2025-08-03 15:32:19 +0300

    Update preAsm.h

--- preAsm.h
+++ preAsm.h
@@ -4,10 +4,10 @@
 int spreadMacros(int , char*[] , char***);
 int isValidName(char*);
 int lineContainsEndAndValid(char[]);
-void copyIntoFile(int, char*[], FILE*, int*, int, char**, char**);
+void copyIntoFile(int, char*[], FILE*, int[], int, char**, char**);
 int* loadMacroIntoTables(char***, char***, int, char*[]);
 char* getLineFromFile(FILE*, char[], char* , int);
-char* trim(char*);
+char* removeStartEndSpaces(char*);
 extern const char commands[16][4];
 
-#endif 
+#endif
