5c21cc6088220888a4d17eba84a789bca10f37ce
eilamLvsInhrtnc <gaziteilam1@gmail.com>
2025-08-07 17:16:43 +0300

    Update firstPass.h

--- firstPass.h
+++ firstPass.h
@@ -1,22 +1,14 @@
 #ifndef FIRSTPASS_H
 #define FIRSTPASS_H
 
-typedef struct Symbol {
-    char *label;
-    int adress;
-    char *labelType;
-}Symbol;
+#include <stdio.h>
+#include "util.h"
 
-extern int symbolIdx;
-extern int errorCode;
-extern const int ICF;
-extern const int DCF;
 int isValidLabel(char* , char** , Symbol* , int);
 int isReg(char*);
 int countWordsForCode(char * , int);
 int countWordsForData(char * , int);
-Symbol* firstPass(int argc , char *argv[]);
+Symbol* firstPass(char * , char *);
 int getExpectedOperandsCount(char *);
-char *getBinaryRep(char *line);
 
 #endif
