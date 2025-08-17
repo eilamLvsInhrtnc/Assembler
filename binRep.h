6ef144ad846fa0edfe1efe6d8c39f7c6a7df4a41
eilamLvsInhrtnc <gaziteilam1@gmail.com>
2025-08-17 23:35:33 +0300

    Update binRep.h

--- binRep.h
+++ binRep.h
@@ -1,42 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include "preAsm.h"
-#include "firstPass.h"
-#include "util.h"
-#include "binRep.h"
-#include "secondPass.h"
-int main(int argc, char *argv[]) {
+#ifndef BINREP_H
+#define BINREP_H
 
-    if (argc < 2) {  // No input files provided
-        fprintf(stderr, "%s: No input files.\n", argv[0]);
-        return 1;
-    }
-    char *argvAM[argc];
-    argvAM[0] = argv[0]; // Keep the program name
-    for (int i = 1; i < argc; i++) {
-        argvAM[i] = preAsmFileName(argv[i]); // Convert each argument to .am file name
-    }
-    for (int fileIdx = 1; fileIdx < argc; fileIdx++) {
-        errorCode = 0; // Reset error code for each file
-        symbolIdx = 0;
-        binRepIdx = 0;
-        firstPass(argvAM[fileIdx], argv[fileIdx]); // Perform first pass assembly
-        if (errorCode != 0) {
-            printf("Error in first pass for file %s.\n", argv[fileIdx]);
-            continue; // Skip to next file if error occurred
-        }
-        if (symbolTable == NULL) {
-            printf("No symbols found in file %s.\n", argv[fileIdx]);
-            return 1;
-        }
-        for (int i = 0; i < symbolIdx; i++) {
-            printf("Symbol: %s, Address: %d, Type: %s\n", symbolTable[i].label, symbolTable[i].adress, symbolTable[i].labelType);
-        }
-        for (int j = 0; j < binRepIdx; j++) {
-            printf("%s", binRep[j].binaryString);
-        }
-    }
-    return 0;
-}
+#include <stdio.h>
+#include "util.h"
+
+void buildFirstWord(const Opcode* , int , int , char*);
+void packImmediate(int, char*);
+void packTwoRegisters(char* , char* , char*);
+void packSingleRegister(char* , char*);
+void extractMatrixRegs(char* , char* , char*);
+int getAdressType(char*);
+const Opcode* getOpcode(char*);
+void appendOperand(char*, char*, int, int, char*, int);
+
+char* dataToBinary(char* , int , int);
+char* codeToBinary(char*);
+
+#endif
