fa60c170d0707096e17c54898e6aea54ae05b1b6
eilamLvsInhrtnc <gaziteilam1@gmail.com>
2025-08-17 23:40:18 +0300

    Update util.c

--- util.c
+++ util.c
@@ -3,27 +3,37 @@
 #include <string.h>
 #include <ctype.h>
 #include "util.h"
+
 int errorCode = 0;
 int symbolIdx = 0;
+int binRepIdx = 0;
+int ICF = 0;
+int DCF = 0;
+int extCount = 0;
+int entryCount = 0;
+Symbol *symbolTable = NULL;
+Symbol *extTable = NULL;
+BinRep *binRep = NULL;
+
 const Opcode opcodes[16] = {
-    { "mov",  "0000" },
-    { "cmp",  "0001" },
-    { "add",  "0010" },
-    { "sub",  "0011" },
-    { "not",  "0100" },
-    { "clr",  "0101" },
-    { "lea",  "0110" },
-    { "inc",  "0111" },
-    { "dec",  "1000" },
-    { "jmp",  "1001" },
-    { "bne",  "1010" },
-    { "red",  "1011" },
-    { "prn",  "1100" },
-    { "jsr",  "1101" },
-    { "rts",  "1110" },
-    { "stop", "1111" }
+    { "mov",  "0000" , 2 , "0123" , "123"},
+    { "cmp",  "0001" , 2 , "0123" , "0123"},
+    { "add",  "0010" , 2 , "0123" , "123"},
+    { "sub",  "0011" , 2 , "0123" , "123"},
+    { "lea",  "0100" , 2 , "12" , "123"},
+    { "clr",  "0101" , 1 , "-" , "123"},
+    { "not",  "0110" , 1 , "-" , "123"},
+    { "inc",  "0111" , 1 , "-" , "123"},
+    { "dec",  "1000" , 1 , "-" , "123"},
+    { "jmp",  "1001" , 1 , "-" , "123"},
+    { "bne",  "1010" , 1 , "-" , "123"},
+    { "jsr",  "1011" , 1 , "-" , "123"},
+    { "red",  "1100" , 1 , "-" , "123"},
+    { "prn",  "1101" , 1 , "-" , "0123"},
+    { "rts",  "1110" , 0 , "-" , "-"},
+    { "stop", "1111" , 0 , "-" , "-"}
 };
-const char commands[16][4] = { "mov" , "cmp" , "add" , "sub" , "not" , "clr" ,"lea" , "inc" , "dec" , "jmp" , "bne", "red" ,"prn" ,"jsr" , "rts" , "stop"};
+const char commands[16][4] = { "mov" , "cmp" , "add" , "sub" , "lea" , "clr" ,"not" , "inc" , "dec" , "jmp" , "bne", "jsr" ,"red" ,"prn" , "rts" , "stop"};
 
 /**
  * @param str string to trim
@@ -77,12 +87,38 @@ char* getLineFromFile(FILE* fp, char line[], char* fileName, int *lineCounter) {
     }
     return line;
 }
-char* decToBinary10Bit(int num) {
-    char binaryString[11];
-    for (int i = 9; i >= 0; i--) {
-        binaryString[9 - i] = ((num >> i) & 1) ? '1' : '0';
+// Convert number to 8-bit binary string (two's complement for negative)
+void decToBinary8Bit(int num, char* bin) {
+    unsigned char val = (unsigned char)num;
+    for (int i = 7; i >= 0; i--) {
+        bin[7 - i] = ((val >> i) & 1) ? '1' : '0';
     }
-    binaryString[10] = '\0'; // Null-terminate the string
-    printf("%s\n" , binaryString);
-    return strdup(binaryString);
+    bin[8] = '\0';
+}
+
+// Convert int val (0-1023) to 10-bit binary string in bin buffer (must hold 11 chars)
+void intToBinary10Bit(int val , char* bin) {
+    for (int i = 9; i >= 0; i--) {
+        bin[9 - i] = ((val >> i) & 1) ? '1' : '0';
+    }
+    bin[10] = '\0';
+}
+
+void binary10BitToBase4FiveBit(char *binaryString , char *buffer) {
+    for (int i = 0; i < 5; i++) {
+        char b1 = binaryString[2*i];
+        char b2 = binaryString[2*i + 1];
+        int value = (b1 - '0') * 2 + (b2 - '0');
+        buffer[i] = 'a' + value; // map 0->a, 1->b, 2->c, 3->d
+    }
+    buffer[5] = '\0';
+}
+
+void decToBase4FourBits(int number, char *buffer) {
+    char nums[4] = {'a','b','c','d'};
+    for (int i = 3; i >= 0; i--) {
+        buffer[i] = nums[number % 4];
+        number /= 4;
+    }
+    buffer[4] = '\0';
 }
