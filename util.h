02dc3df6cb19a60f68b9c12c40c802f54d5300d0
eilamLvsInhrtnc <gaziteilam1@gmail.com>
2025-08-18 13:18:11 +0300

    Update util.h

--- util.h
+++ util.h
@@ -18,39 +18,39 @@ void decToBinary8Bit(int , char*);
 void binary10BitToBase4FiveBit(char* , char*);
 void decToBase4FourBits(int , char*);
 
-extern const char commands[16][4];
-extern int errorCode;
-extern int symbolIdx;
-extern int binRepIdx;
-extern int ICF;
-extern int DCF;
-extern int extCount;
-extern int entryCount;
+extern const char commands[16][4]; // global array of commands
+extern int errorCode; // global error code
+extern int symbolIdx; // global symbolTable index
+extern int binRepIdx; // global binRep index
+extern int ICF; // global final instruction counter
+extern int DCF; // global final data counter
+extern int extCount; // global external symbols count
+extern int entryCount; // global entry symbols count
 
-typedef struct Symbol {
-    char *label;
-    int adress;
-    char *labelType;
+typedef struct Symbol { // symbol struct to hold labels.
+    char *label; // label name
+    int adress; // address of the label
+    char *labelType; // type of the label (code , data , entry , external)
 }Symbol;
 
-typedef struct Opcode {
-    char *opcode;
-    char *binaryRep;
+typedef struct Opcode { // opcode struct to hold opcode information.
+    char *opcode; // Opcode name
+    char *binaryRep; // 4-bit binary representation of the opcode
     int expectedOperands; // Number of expected operands for the opcode
-    char *operandN1legalAddressingModes;
-    char *operandN2legalAddressingModes;
+    char *operandN1legalAddressingModes; // Legal addressing modes for operand 1
+    char *operandN2legalAddressingModes; // Legal addressing modes for operand 2
 }Opcode;
 
 typedef struct BinRep {
-    char *binaryString;
-    char *lineType;
+    char *binaryString; // Binary representation of the instruction
+    char *lineType; // Type of the line (code , data)
     int lineNumber; // Line number in the source file
-    int address; // address
+    int address; // address of the line (current IC or current DC)
 }BinRep;
 
-extern const Opcode opcodes[16];
-extern Symbol *symbolTable;
-extern Symbol *extTable;
-extern BinRep *binRep;
+extern const Opcode opcodes[16]; // global map of opcodes
+extern Symbol *symbolTable; // global symbol table
+extern Symbol *extTable; // global external symbols table
+extern BinRep *binRep; // global binary representation table
 
 #endif
