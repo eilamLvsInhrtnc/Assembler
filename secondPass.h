bc7aa7a4cd138ce7dfea9d663f5464183577ce89
eilamLvsInhrtnc <gaziteilam1@gmail.com>
2025-08-19 13:37:37 +0300

    Update secondPass.h

--- secondPass.h
+++ secondPass.h
@@ -1,6 +1,9 @@
+/*
+    *Eilam Gazit , Eyal Hets Cohen.
+    *secondPass.h - header file for secondPass.c
+*/
 #ifndef SECONDPASS_H
 #define SECONDPASS_H
-
 
 void secondPass(char *);
 char* objectFileName(char *);
